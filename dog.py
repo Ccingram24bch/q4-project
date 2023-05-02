11ba788da884b0cd23aa6171b828d71a86a89b9b
Ccingram24bch <126904938+Ccingram24bch@users.noreply.github.com>
2023-05-02 11:33:07 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,10 +1,10 @@
 class Dog:
-    dog.happiness = 100
-    dog.health = 100
-    dog.hunger = 100
-    dog.thirst = 100
-    dog.x = 100
-    dog.y = 100
-    dog.height = 64
-    dog.width = 64
+    dog.happiness = happiness
+    dog.health = health
+    dog.hunger = hunger
+    dog.thirst = thirst
+    dog.x = x
+    dog.y = y
+    dog.height = height
+    dog.width = width
 test test
