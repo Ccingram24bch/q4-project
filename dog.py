42052f7bd942a6fac9fc631385cfe31a81a08e8a
Lstel6 <130575238+Lstel6@users.noreply.github.com>
2023-05-02 11:35:47 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,10 +1,10 @@
 class Dog:
-    def __init__(self, happiness, health, hunger, thirst, x, y, height, width)
-    dog.happiness = happiness
-    dog.health = health
-    dog.hunger = hunger
-    dog.thirst = thirst
-    dog.x = x
-    dog.y = y
-    dog.height = height
-    dog.width = width
+    def __init__(self, happiness, health, hunger, thirst, x, y, height, width):
+        dog.happiness = happiness
+        dog.health = health
+        dog.hunger = hunger
+        dog.thirst = thirst
+        dog.x = x
+        dog.y = y
+        dog.height = height
+        dog.width = width
