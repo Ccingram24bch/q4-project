9d3afd282a6586eaba5b3bfc37217b4a479dc5b0
Ccingram24bch <126904938+Ccingram24bch@users.noreply.github.com>
2023-05-31 09:18:53 -0400

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,6 +1,7 @@
 import turtle
 import random
 
+
 pet = {'name': "", 'breed': "", 'age': 0}
 
 # for the breed figure out how to make the colors in turtle graphics, so the breed chosen determines its color
@@ -8,16 +9,17 @@ pet = {'name': "", 'breed': "", 'age': 0}
 breed = {'Yellow Lab', 'Chocolate Lab', 'German Shepherd', 'Shih Tzu'}
 
 class Dog:
-    def __init__(self, name, happiness, health, hunger, thirst, x, y, breed, age):
+    def __init__(self, name, happiness, max_health, current_health, max_hunger, current_hunger, max_thirst, current_thirst, x, y, breed):
         self.name = name
-        self.happiness = happiness
-        self.health = health
-        self.hunger = hunger
-        self.thirst = thirst
+        self.max_health = 100
+        self.current_health = 100
+        self.max_hunger = 100
+        self.current_hunger = 100
+        self.max_thirst = 100
+        self.current_thirst = 100
         self.x = x
         self.y = y
         self.breed = breed
-        self.age = age
 
 def Petchoice():
     petBreed = ""
@@ -25,136 +27,91 @@ def Petchoice():
     while petBreed not in breed:
         print(breed)
         petBreed = input('Enter which dog breed from the list you would like: ')
-        if petBreed == "Shi Tzu":
+        if petBreed == "Shih Tzu":
             dog = turtle.Turtle()
             dog.penup()
             screen = turtle.Screen()
-            screen.setup(1000,1000)
-            image = 'ShiTzu.jpg'
+            screen.setup(1000, 1000)
+            image = 'test.gif'
             screen.addshape(image)
             screen.bgpic(image)
+            image1 = 'Dog1.gif'
+            screen.addshape(image1)
+            dog.shape(image1)
+            dog.speed('fastest')
+
+        elif petBreed == "German Shepherd":
+            dog = turtle.Turtle()
+            dog.penup()
+            screen = turtle.Screen()
+            screen.setup(1000, 1000)
+            image = 'test.gif'
+            screen.addshape(image)
+            screen.bgpic(image)
+            image1 = 'GermanShepherd.gif'
+            screen.addshape(image1)
+            dog.shape(image1)
+            dog.speed('fastest')
+
+
+
+
+
+
+
+        def up():
+            dog.setheading(90)
+            dog.forward(75)
+
+        def down():
+            dog.setheading(270)
+            dog.forward(75)
+
+        def left():
+            dog.setheading(180)
+            dog.forward(75)
+
+        def right():
+            dog.setheading(0)
+            dog.forward(75)
+
+        def Upright():
+            dog.setheading(45)
+            dog.forward(75)
+
+        def Upleft():
+            dog.setheading(135)
+            dog.forward(75)
+
+        def Downleft():
+            dog.setheading(225)
+            dog.forward(75)
+
+        def Downright():
+            dog.setheading(315)
+            dog.forward(75)
+
+        turtle.listen()
+
+        turtle.onkey(up, "Up")
+        turtle.onkey(down, "Down")
+        turtle.onkey(left, "Left")
+        turtle.onkey(right, "Right")
+        turtle.onkey(Upright, "'")
+        turtle.onkey(Upleft, ";")
+        turtle.onkey(Downleft, '.')
+        turtle.onkey(Downright, '/')
+
 
     pet['breed'] = petBreed
-    pet['name'] = input('What is the name of your ' + pet['breed'] + '?: ')
-
-
+   # pet['name'] = input('What is the name of your ' + pet['breed'] + '?: ')
 Petchoice()
 
-screen = turtle.Screen()
-tim = turtle.Turtle()
-tim.penup()
 
 
-# sprite integration
-# sprite = "...Shih-Tzu-On-White-01.jpg"
-
-# screen.addshape(sprite)
-# dog.shape(sprite)
-
-def up():
-    tim.setheading(90)
-    tim.forward(100)
-    x,y = tim.position()
-    if tim.ycor() >= 600:
-        UP_PAGE = True
-        DOWN_PAGE = False
-        LEFT_PAGE = False
-        RIGHT_PAGE = False
-        if UP_PAGE == True:
-            screen.setup(1000,500)
-            tim.setpos(-350,10)
-        #WALK GAME HERe
-        #FALSIFY UP PAGE
-
-def down():
-    tim.setheading(270)
-    tim.forward(100)
-    if tim.ycor() <= -1000:
-        UP_PAGE = False
-        DOWN_PAGE = True
-        LEFT_PAGE = False
-        RIGHT_PAGE = False
-        if DOWN_PAGE == True:
-            screen.setup(1000, 500)
-            tim.setpos(-350, 10)
-        #TRICK GAME?
-
-def left():
-    tim.setheading(180)
-    tim.forward(100)
-    if tim.xcor() <= -1000:
-        UP_PAGE = False
-        DOWN_PAGE = False
-        LEFT_PAGE = True
-        RIGHT_PAGE = False
-        if LEFT_PAGE == True:
-            screen.setup(1000, 500)
-            tim.setpos(-350, 10)
-        #UPDATE WATER OR HUNGER
-
-def right():
-    tim.setheading(0)
-    tim.forward(100)
-    if tim.xcor() <= 1000:
-        screen.setup(500, 5000)
-        tim.setpos(100, 100)
-        #UPDATE WATER OR HUNGER
-
-def Upright():
-    tim.setheading(45)
-    tim.forward(100)
-
-def Upleft():
-    tim.setheading(135)
-    tim.forward(100)
-
-def Downleft():
-    tim.setheading(225)
-    tim.forward(100)
-
-def Downright():
-    tim.setheading(315)
-    tim.forward(100)
 
 
-colors = ["red", "blue", "green", "yellow", "black"]
 
-
-def clickLeft(x, y):
-    tim.color(random.choice(colors))
-
-
-def clickRight(x, y):
-    tim.stamp()
-
-
-turtle.listen()
-
-turtle.onscreenclick(clickLeft, 1)
-turtle.onscreenclick(clickRight, 3)
-
-
-turtle.onkey(up, "Up")
-turtle.onkey(down, "Down")
-turtle.onkey(left, "Left")
-turtle.onkey(right, "Right")
-turtle.onkey(Upright, "'")
-turtle.onkey(Upleft, ";")
-turtle.onkey(Downleft, '.')
-turtle.onkey(Downright, '/')
 
 turtle.mainloop()
 
-#creating images of the Shi Tzu and the Grass background
-tim = turtle.Turtle()
-tim.penup()
-screen = turtle.Screen()
-screen.setup(1000,1000)
-image = 'test.gif'
-screen.addshape(image)
-screen.bgpic(image)
-image1 = 'Dog1.gif'
-screen.addshape(image1)
-tim.shape(image1)
-
-tim.speed(10)
